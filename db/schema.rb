3291a26b368b052f6345ad11b8e3e2d6a63ea2c7
mosesakor <gabrielmosesakor@gmail.com>
2022-01-28 17:45:57 +0100

    updated database.yml file

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_01_16_232959) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -24,11 +27,11 @@ ActiveRecord::Schema.define(version: 2022_01_16_232959) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
-    t.integer "article_id", null: false
+    t.bigint "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "status"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
