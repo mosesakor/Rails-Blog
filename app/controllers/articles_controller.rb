c5e03437b2ac140c4b4ca81b24edc4812ec54650
mosesakor <gabrielmosesakor@gmail.com>
2022-02-02 14:29:50 +0100

    added site image to boost aesthetics of site

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
   skip_before_action :verify_authenticity_token
   def index
     @articles = Article.all
+    @homepage = true
   end
 
   def show
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,4 @@ module ApplicationHelper
 #       options = [:hard_wrap, :autolink, :no_intra_emphasis, :fenced_code_blocks]
 #        Markdown.new(text, *options).to_html.html_safe
 #    end
-
 end
