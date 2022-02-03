29731c20643e2d1d299ea93d8c1ad37b548aa461
mosesakor <gabrielmosesakor@gmail.com>
2022-02-03 10:51:51 +0100

    added trix manually wiht yarn

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Entry point for the build script in your package.json
 import * as bootstrap from "bootstrap"
-//import "trix"
-//import "@rails/actiontext"
-require("trix")
-require("@rails/actiontext")
+import "trix"
+import "@rails/actiontext"
+//require("trix")
+//require("@rails/actiontext")
